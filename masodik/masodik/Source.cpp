7b0ed6e6eb8b18f29237b47eed904a64d3a747a7
László Nagyenyedi <laszlo.nagyenyedi@gmail.com>
2022-04-08 21:37:06 +0200

    Removed comments

--- masodik/masodik/Source.cpp
+++ masodik/masodik/Source.cpp
@@ -1,9 +1,4 @@
-﻿/*
-https://en.wikipedia.org/wiki/B%C3%A9zier_curve#Explicit_definition
-https://en.wikipedia.org/wiki/Factorial
-https://hu.wikipedia.org/wiki/Binomi%C3%A1lis_egy%C3%BCtthat%C3%B3
-*/
-#include <array>
+﻿#include <array>
 #include <fstream>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
@@ -28,7 +23,6 @@ std::vector<glm::vec3> myControlPoints = {
 	glm::vec3(0.5f,  0.5f, 0.0f),
 };
 
-/* Vertex buffer objektum és vertex array objektum az adattároláshoz.*/
 #define numVBOs 1
 #define numVAOs 1
 GLuint VBO[numVBOs];
@@ -120,7 +114,6 @@ GLuint createShaderProgram()
 		printShaderLog(fShader);
 	}
 
-	// Shader program objektum létrehozása. Eltároljuk az ID értéket.
 	GLuint vfProgram = glCreateProgram();
 	glAttachShader(vfProgram, vShader);
 	glAttachShader(vfProgram, fShader);
@@ -155,26 +148,8 @@ void framebufferSizeCallback(GLFWwindow* window, int width, int height) {
 	glViewport(0, 0, width, height);
 }
 
-/*
-The definition of N choose R is to compute the two productsand divide one with the other,
-(N * (N - 1) * (N - 2) * ... * (N - R + 1)) / (1 * 2 * 3 * ... * R)
-However, the multiplications may become too large really quickand overflow existing data type.The implementation trick is to reorder the multiplicationand divisions as,
-(N) / 1 * (N - 1) / 2 * (N - 2) / 3 * ... * (N - R + 1) / R
-It's guaranteed that at each step the results is divisible (for n continuous numbers, one of them must be divisible by n, so is the product of these numbers).
-For example, for N choose 3, at least one of the N, N - 1, N - 2 will be a multiple of 3, and for N choose 4, at least one of N, N - 1, N - 2, N - 3 will be a multiple of 4.
-C++ code given below.
-*/
 int NCR(int n, int r) {
-	/*
-	binomial coefficient
-	*/
 	if (r == 0) return 1;
-
-	/*
-	 Extra computation saving for large R,
-	 using property:
-	 N choose R = N choose (N - R)
-	*/
 	if (r > n / 2) return NCR(n, n - r);
 
 	long res = 1;
@@ -187,20 +162,14 @@ int NCR(int n, int r) {
 	return res;
 }
 
-/*
-It will be the Bernstein basis polynomial of degree n.
-*/
 GLfloat blending(GLint n, GLint i, GLfloat t) {
 	return NCR(n, i) * pow(t, i) * pow(1.0f - t, n - i);
 }
 
-void drawBezierCurve(std::vector<glm::vec3> controlPoints) {
-	/*
-	https://en.wikipedia.org/wiki/B%C3%A9zier_curve#Explicit_definition
-	*/
+void generateBezierCurve(std::vector<glm::vec3> controlPoints) {
 	glm::vec3	nextPoint;
 	GLfloat		t = 0.0f, B;
-	GLfloat		increment = 1.0f / 100.0f; /* hány darab szakaszból rakjuk össze a görbénket? */
+	GLfloat		increment = 1.0f / 100.0f;
 
 	while (t <= 1.0f) {
 		nextPoint = glm::vec3(0.0f, 0.0f, 0.0f);
@@ -218,106 +187,57 @@ void drawBezierCurve(std::vector<glm::vec3> controlPoints) {
 
 void init(GLFWwindow* window) {
 	renderingProgram = createShaderProgram();
-
-	drawBezierCurve(myControlPoints);
-
-	/* Létrehozzuk a szükséges Vertex buffer és vertex array objektumot. */
+	generateBezierCurve(myControlPoints);
 	glGenBuffers(numVBOs, VBO);
 	glGenVertexArrays(numVAOs, VAO);
-
-	/* Típus meghatározása: a GL_ARRAY_BUFFER nevesített csatolóponthoz kapcsoljuk a buffert (ide kerülnek a vertex adatok). */
 	glBindBuffer(GL_ARRAY_BUFFER, VBO[0]);
-
-	/* Másoljuk az adatokat a pufferbe! Megadjuk az aktuálisan csatolt puffert,  azt hogy hány bájt adatot másolunk,
-	a másolandó adatot, majd a feldolgozás módját is meghatározzuk: most az adat nem változik a feltöltés után */
 	glBufferData(GL_ARRAY_BUFFER, pointToDraw.size() * sizeof(glm::vec3), pointToDraw.data(), GL_STATIC_DRAW);
-
-	/* Csatoljuk a vertex array objektumunkat a konfiguráláshoz. */
 	glBindVertexArray(VAO[0]);
-
-	/* Ezen adatok szolgálják a 0 indexű vertex attribútumot (itt: pozíció).
-	Elsőként megadjuk ezt az azonosítószámot.
-	Utána az attribútum méretét (vec3, láttuk a shaderben).
-	Harmadik az adat típusa.
-	Negyedik az adat normalizálása, ez maradhat FALSE jelen példában.
-	Az attribútum értékek hogyan következnek egymás után? Milyen lépésköz után találom a következő vertex adatait?
-	Végül megadom azt, hogy honnan kezdődnek az értékek a pufferben. Most rögtön, a legelejétől veszem őket.*/
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-
-	/* Engedélyezzük az imént definiált 0 indexű attribútumot. */
 	glEnableVertexAttribArray(0);
-
-	/* Leválasztjuk a vertex array objektumot és a puffert is.*/
 	glBindVertexArray(0);
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-	// aktiváljuk a shader-program objektumunkat.
 	glUseProgram(renderingProgram);
-
 	glClearColor(0.0, 0.0, 0.0, 1.0);
 }
 
-/** A jelenetünk utáni takarítás. */
 void cleanUpScene() {
-	/** Töröljük a vertex puffer és vertex array objektumokat. */
 	glDeleteVertexArrays(numVAOs, VAO);
 	glDeleteBuffers(numVBOs, VBO);
-
-	/** Töröljük a shader programot. */
 	glDeleteProgram(renderingProgram);
 }
 
 void display(GLFWwindow* window, double currentTime) {
-	glClear(GL_COLOR_BUFFER_BIT); // fontos lehet minden egyes alkalommal törölni!
-
-	/*Csatoljuk a vertex array objektumunkat. */
+	glClear(GL_COLOR_BUFFER_BIT);
 	glBindVertexArray(VAO[0]);
 
 	glLineWidth(2.0f);
 	glDrawArrays(GL_LINE_STRIP, 0, pointToDraw.size());
 
-	/* Leválasztjuk, nehogy bármilyen érték felülíródjon.*/
+
 	glBindVertexArray(0);
 }
 
 int main(void) {
-	/* Próbáljuk meg inicializálni a GLFW-t! */
 	if (!glfwInit()) { exit(EXIT_FAILURE); }
-
-	/* A kívánt OpenGL verzió (4.3) */
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-
-	/* Próbáljuk meg létrehozni az ablakunkat. */
 	GLFWwindow* window = glfwCreateWindow(window_width, window_height, window_title, NULL, NULL);
-
-	/* Válasszuk ki az ablakunk OpenGL kontextusát, hogy használhassuk. */
 	glfwMakeContextCurrent(window);
-
 	glfwSetKeyCallback(window, keyCallback);
 	glfwSetCursorPosCallback(window, cursorPosCallback);
 	glfwSetMouseButtonCallback(window, mouseButtonCallback);
 	glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
-
-	/* Incializáljuk a GLEW-t, hogy elérhetővé váljanak az OpenGL függvények. */
 	if (glewInit() != GLEW_OK) { exit(EXIT_FAILURE); }
 	glfwSwapInterval(1);
-
-	/* Az alkalmazáshoz kapcsolódó előkészítő lépések, pl. hozd létre a shader objektumokat. */
 	init(window);
 
 	while (!glfwWindowShouldClose(window)) {
-		/* a kód, amellyel rajzolni tudunk a GLFWwindow ojektumunkba. */
 		display(window, glfwGetTime());
-		/* double buffered */
 		glfwSwapBuffers(window);
-		/* események kezelése az ablakunkkal kapcsolatban, pl. gombnyomás */
 		glfwPollEvents();
 	}
-
-	/* töröljük a GLFW ablakot. */
 	glfwDestroyWindow(window);
-	/* Leállítjuk a GLFW-t */
 
 	cleanUpScene();
 
