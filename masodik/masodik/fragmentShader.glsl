63fdb8d4ec22f64add52e72ca64b5937d994e44a
László Nagyenyedi <laszlo.nagyenyedi@gmail.com>
2022-04-08 22:34:36 +0200

    Multiple changes
    - 3.1 working
    - 3.2 working

--- masodik/masodik/fragmentShader.glsl
+++ masodik/masodik/fragmentShader.glsl
@@ -1,6 +1,7 @@
 #version 430
 
 in float myIsPoint;
+in float myIsPolygon;
 
 out vec4 color;
 
@@ -11,6 +12,8 @@ void main() {
          if(length(gl_PointCoord-vec2(0.5)) > 0.5)
             discard;
         color = vec4(0.0,1.0,0.0,1.0);
+    }else if(myIsPolygon>0.5){
+                color = vec4(0.0,0.0,1.0,1.0);
     }
     else{
         color = vec4(1.0,0.0,0.0,1.0);
